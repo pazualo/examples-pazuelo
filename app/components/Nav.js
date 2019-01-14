fcdbc9d5b2ba585075e26c90a22c844c0d3d3981
Matan P <matan.pazuelo@danhotels.com>
2019-01-14 21:04:26 +0200

    added github link, added sw.js, added favicon

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { NavLink } from 'react-router-dom';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { NavLink } from 'react-router-dom'
+import { relative } from 'path'
 const Nav = () => {
   return (
-    <ul className="nav">
+    <ul className="nav" style={{ position: 'relative' }}>
       <li style={{ marginRight: 15, listStyleType: 'none' }}>
         <NavLink exact activeClassName="active" to="/">
           Home
@@ -19,7 +20,24 @@ const Nav = () => {
           Popular
         </NavLink>
       </li>
+      <li
+        style={{
+          marginRight: 15,
+          listStyleType: 'none',
+          position: 'absolute',
+          right: '10px',
+          top: '5px'
+        }}
+      >
+        <a href="https://github.com/pazualo" target="_blank">
+          <img
+            src="https://image.flaticon.com/icons/svg/270/270798.svg"
+            alt="github icon"
+            width="20px"
+          />
+        </a>
+      </li>
     </ul>
-  );
-};
-export default Nav;
+  )
+}
+export default Nav
